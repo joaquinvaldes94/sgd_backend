7834ae5f7228ea1f3a9c2d7d12a1222976128edc
Joaquin Valdes C <joaquinvaldesc@gmail.com>
2024-04-05 21:31:52 -0300

    se agrega nueva configuracion para uso de proyecto

--- src/main/java/cl/nescorp/provider/aplication/controller/DemoController.java
+++ src/main/java/cl/nescorp/provider/aplication/controller/DemoController.java
@@ -1,6 +1,8 @@
 package cl.nescorp.provider.aplication.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Hidden
 public class DemoController {
 
-  @GetMapping
+	@GetMapping(value="/pruebaApp",produces = {MediaType.APPLICATION_JSON_VALUE})
   public ResponseEntity<String> sayHello() {
     return ResponseEntity.ok("Hello from secured endpoint");
   }
