b861ff321dd2c2525386c6fd1f4dc486bf255b77
Jean R <jereyesal@falabella.cl>
2024-07-23 14:28:26 -0400

    coors

---
+++ src/main/java/cl/nescorp/provider/aplication/config/WebConfig.java
@@ -0,0 +1,24 @@
+package cl.nescorp.provider.aplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite CORS en todas las rutas
+                        .allowedOrigins("http://localhost:3000", "https://front-dev.nes-sgd.cl", "http://front-dev.nes-sgd.cl", "https://front.nes-sgd.cl", "http://front.nes-sgd.cl") // Origen permitido
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos los headers
+                        .allowCredentials(true); // Permite cookies y datos de autenticación
+            }
+        };
+    }
+}
